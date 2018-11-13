c7b1593ef28011bb73d9edff945f2d6341c1a4bb
Hamed Iravanchi <iravanchi@gmail.com>
2018-11-13 20:04:55 +0330

    Add testsettings.json and simple code to read it - to be used for ApiKey

--- test/Proxy.Tests/IaaS/GetFloatingIpListTests.cs
+++ test/Proxy.Tests/IaaS/GetFloatingIpListTests.cs
@@ -1,10 +1,18 @@
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Xunit;
 
 namespace Arvan.Proxy.Tests.IaaS
 {
     public class GetFloatingIpListTests
     {
+        private  readonly string _settingsField;
+        
+        public GetFloatingIpListTests()
+        {
+            _settingsField = GetApplicationConfiguration();
+        }
+
         [Fact]
         public async Task CheckInvocationWithoutErrors()
         {
@@ -12,6 +20,22 @@ namespace Arvan.Proxy.Tests.IaaS
             var result = await arvan.IaaS.GetFloatIpList();
             
             Assert.NotNull(result);
+        }
+        
+        public static IConfigurationRoot GetIConfigurationRoot()
+        {            
+            return new ConfigurationBuilder()
+                .AddJsonFile("testsettings.json", false)
+                .AddJsonFile("testsettings.devlocal.json", false)
+                .AddUserSecrets("883cd70e-d90b-41fc-a7dd-47695db0d616")
+                .AddEnvironmentVariables()
+                .Build();
+        }
+        
+        public static string GetApplicationConfiguration()
+        {
+            var iConfig = GetIConfigurationRoot();
+            return iConfig.GetSection("SomeField").GetValue<string>("AnotherField");
         }
     }
 }
