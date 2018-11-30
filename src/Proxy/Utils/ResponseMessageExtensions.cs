dede0af100de3120132fe2e84020055fbc44e686
Hamed Iravanchi <iravanchi@gmail.com>
2018-12-01 00:59:48 +0330

    Better error results, deserialize error message returned by the server when the status code is a failure.

--- src/Proxy/Utils/ResponseMessageExtensions.cs
+++ src/Proxy/Utils/ResponseMessageExtensions.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Net;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Hydrogen.General.Utils;
@@ -10,22 +10,22 @@ namespace Arvan.Proxy.Utils
 {
     public static class ResponseMessageExtensions
     {
-        public static ApiValidationResult ToValidationResult(this HttpResponseMessage response)
+        public static async Task<ApiValidationResult> ToValidationResult(this HttpResponseMessage response)
         {
             return response.IsSuccessStatusCode
                 ? ApiValidationResult.Ok()
-                : ApiValidationResult.Failure(response.ToValidationError());
+                : await response.DeserializeFailureAsync();
         }
 
         public static async Task<ApiValidatedResult<TResponse>> ToValidatedResult<TResponse>(this HttpResponseMessage response)
         {
             if (response.IsSuccessStatusCode)
             {
-                var content = await response.DeserializeAsync<TResponse>();
+                var content = await response.DeserializeSuccessAsync<TResponse>();
                 return ApiValidatedResult<TResponse>.Ok(content);
             }
 
-            return ApiValidatedResult<TResponse>.Failure(response.ToValidationError());
+            return await response.DeserializeFailureAsync<TResponse>();
         }
 
         public static async Task<ApiValidatedResult<string>> ToRawValidatedResult(this HttpResponseMessage response)
@@ -39,20 +39,11 @@ namespace Arvan.Proxy.Utils
             return ApiValidatedResult<string>.Failure(await response.Content.ReadAsStringAsync());
         }
 
-        public static ApiValidationError ToValidationError(this HttpResponseMessage response)
-        {
-            return new ApiValidationError(response.StatusCode.ToString(), response.ReasonPhrase.Yield());
-        }
-
-        public static async Task<T> DeserializeAsync<T>(this HttpResponseMessage self, bool acceptNotFoundResult = false)
+        public static async Task<T> DeserializeSuccessAsync<T>(this HttpResponseMessage self)
         {
             if (!self.IsSuccessStatusCode)
             {
-                if (acceptNotFoundResult && self.StatusCode == HttpStatusCode.NotFound)
-                    return default(T);
-
-                // todo: use a better type rather than ArgumentException
-                throw new ArgumentException($@"the HttpResponseMessage which the type {typeof(T).FullName} is expected to deserialize from was unsuccessful.");
+                throw new ArgumentException($@"HttpResponseMessage is unsuccessful with status code {self.StatusCode} {self.ReasonPhrase}. Cannot deserialize an instance of type {typeof(T).FullName}.");
             }
 
             try
@@ -66,6 +57,62 @@ namespace Arvan.Proxy.Utils
             {
                 throw new InvalidOperationException($@"Error occured while trying to convert HttpResponseMessage to {typeof(T).FullName}", e);
             }
+        }
+        
+        public static async Task<ApiValidationResult> DeserializeFailureAsync(this HttpResponseMessage self)
+        {
+            ApiValidationResult result = null;
+            ApiValidationError deserializationError = null;
+            try
+            {
+                var serializedContent = await self.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<ApiValidationResult>(serializedContent);
+            }
+            catch (Exception e)
+            {
+                deserializationError = new ApiValidationError(e.GetType().Name, e.Message.Yield());
+            }
+            
+            if (result == null)
+                result = new ApiValidationResult();
+            
+            if (result.Errors == null)
+                result.Errors = new List<ApiValidationError>();
+            
+            result.Errors.Add(new ApiValidationError(self.StatusCode.ToString(), self.ReasonPhrase.Yield()));
+            if (deserializationError != null)
+                result.Errors.Add(deserializationError);
+
+            result.Success = false;
+            return result;
+        }
+        
+        public static async Task<ApiValidatedResult<T>> DeserializeFailureAsync<T>(this HttpResponseMessage self)
+        {
+            ApiValidatedResult<T> result = null;
+            ApiValidationError deserializationError = null;
+            try
+            {
+                var serializedContent = await self.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<ApiValidatedResult<T>>(serializedContent);
+            }
+            catch (Exception e)
+            {
+                deserializationError = new ApiValidationError(e.GetType().Name, e.Message.Yield());
+            }
+            
+            if (result == null)
+                result = new ApiValidatedResult<T>();
+            
+            if (result.Errors == null)
+                result.Errors = new List<ApiValidationError>();
+            
+            result.Errors.Add(new ApiValidationError(self.StatusCode.ToString(), self.ReasonPhrase.Yield()));
+            if (deserializationError != null)
+                result.Errors.Add(deserializationError);
+
+            result.Success = false;
+            return result;
         }
     }
 }
